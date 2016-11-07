ae51374923143c00e66987a776fcba08c4ffab60
ysansone <yamila.sansone@mercadolibre.com>
2016-11-07 19:23:52 -0300

    Alta proveedor

--- src/dao/HibernateDAOProveedor.java
+++ src/dao/HibernateDAOProveedor.java
@@ -2,6 +2,12 @@ package dao;
 
 import hbt.HibernateUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.ProveedorDTO;
+import entities.Proveedor;
+
 public class HibernateDAOProveedor extends HibernateDAO {
 	
 	private static HibernateDAOProveedor instancia;
@@ -14,5 +20,45 @@ public class HibernateDAOProveedor extends HibernateDAO {
 		return instancia;
 	}
 	
-
+	@SuppressWarnings("unchecked")
+	public List<ProveedorDTO> obtenerProveedores() {
+		List<ProveedorDTO> proveedoresDTO = new ArrayList<ProveedorDTO>();
+		try {
+			List<Proveedor> proveedores = sessionFactory.openSession().createQuery("FROM Sucursal").list();
+			for (Proveedor proveedor : proveedores) {
+				proveedoresDTO.add(proveedor.toDTO());
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		this.closeSession();
+		return proveedoresDTO;
+	}
+	
+	public ProveedorDTO obtenerProveedorPorId(int id) {
+		ProveedorDTO dto = new ProveedorDTO();
+		try {
+			Proveedor proveedor = obtenerProveedorEntityPorId(id);
+			dto = proveedor.toDTO();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		this.closeSession();
+		return dto;
+	}
+	
+	private Proveedor obtenerProveedorEntityPorId(int id) {
+		Proveedor proveedor = (Proveedor) sessionFactory.openSession().createQuery("FROM Cliente c where c.id=:id")
+				.setParameter("id", id).uniqueResult(); 
+		return proveedor;
+	}
+	
+	public void eliminarProveedor(int id) {
+		Proveedor proveedor = obtenerProveedorEntityPorId(id);
+		borrar(proveedor);
+	}
+	
+	public void modificarProveedor() {
+		
+	}
 }
